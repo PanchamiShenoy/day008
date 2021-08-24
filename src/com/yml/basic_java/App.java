3cc380ca8fc1a035d6071e2252da0a71913a9c0c
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-24 23:49:53 +0530

    to check if its a leap year or not

--- src/com/yml/basic_java/App.java
+++ src/com/yml/basic_java/App.java
@@ -1,10 +1,13 @@
 package com.yml.basic_java;
 
+import com.yml.core_java.LeapYear;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		LeapYear lp=new LeapYear();
+		lp.check();
 	}
 
 }
