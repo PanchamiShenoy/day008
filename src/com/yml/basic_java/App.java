f87d86bccb777491543814b41d37a05bedc0e83e
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-25 07:49:53 +0530

    to calculate Euclidean distance between a point and origin

--- src/com/yml/basic_java/App.java
+++ src/com/yml/basic_java/App.java
@@ -1,10 +1,15 @@
 package com.yml.basic_java;
 
+import com.yml.core_java.*;
+import com.yml.functional.*;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Distance f =new Distance();
+		int x=Integer.parseInt(args[0]); 
+		int y=Integer.parseInt(args[1]);
+		f.check(x,y);
 	}
-
 }
