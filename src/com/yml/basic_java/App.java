6cf72c7e6ee9d70d322e518f5acc3b609a6b0c60
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-25 14:44:39 +0530

    to check if anumber is prime or not

--- src/com/yml/basic_java/App.java
+++ src/com/yml/basic_java/App.java
@@ -2,11 +2,12 @@ package com.yml.basic_java;
 
 import com.yml.core_java.*;
 import com.yml.functional.*;
+import com.yml.logic_program.*;
 
 public class App {
 
 	public static void main(String[] args) {
-		SumOfIntegers si=new SumOfIntegers();
-		si.compute();
+		Prime p=new Prime();
+		p.check();
 	}
 }
