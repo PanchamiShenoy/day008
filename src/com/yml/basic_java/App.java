bb2de1d24bc2d356fce9af9793678cd04e111e3a
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-25 12:48:59 +0530

    find 3 integers in array whose sum is zero

--- src/com/yml/basic_java/App.java
+++ src/com/yml/basic_java/App.java
@@ -6,9 +6,7 @@ import com.yml.functional.*;
 public class App {
 
 	public static void main(String[] args) {
-		int a=Integer.parseInt(args[0]);
-		int b=Integer.parseInt(args[1]);
-		WindChill wc=new WindChill();
-		wc.compute(a, b);
+		SumOfIntegers si=new SumOfIntegers();
+		si.compute();
 	}
 }
