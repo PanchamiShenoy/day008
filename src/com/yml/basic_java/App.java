c437759e99dae5da2b1b43502cb46cf23580948a
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-08-26 21:45:35 +0530

    swapping of nibbles to find new number

--- src/com/yml/basic_java/App.java
+++ src/com/yml/basic_java/App.java
@@ -10,12 +10,7 @@ import com.yml.logic_program.*;
 public class App {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the Number");
-		int num = scanner.nextInt();
-
-		String bits = Util.toBinary(num);
-		System.out.println("After Padding:" + bits);
+		Binary.binary();
 
 	}
 }
---
+++ src/com/yml/junit/Binary.java
@@ -0,0 +1,34 @@
+package com.yml.junit;
+
+import java.util.Scanner;
+import com.yml.junit.Util;
+
+public class Binary {
+
+	public static void binary() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int num = scanner.nextInt();
+		scanner.close();
+		
+		String bits = Util.toBinary(num);
+		
+		
+		
+		String nibble1 = bits.substring(0,4);
+		String nibble2 = bits.substring(4,8);
+		
+		System.out.println("Nibble 1 = " + nibble1 + " Nibble 2 = " + nibble2);
+		String reverse = nibble2 + nibble1;
+		System.out.println("Reversed Binary = " + reverse);
+		int result = 0;
+		
+		for(int i=reverse.length()-1;i>=0;i--) {
+			int bit = Character.getNumericValue(reverse.charAt(i));
+			result = (int) (result + (bit * Math.pow(2, 7-i)));
+		}
+		System.out.println("The Reversed Binary Equivalent Decimal Value = " + result);
+	}
+	
+	
+}
