9ce5fbe5416df74dcf20241b53ccb64247a2ccb5
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-08-26 21:07:47 +0530

    toBinary is modified

--- src/com/yml/basic_java/App.java
+++ src/com/yml/basic_java/App.java
@@ -10,8 +10,12 @@ import com.yml.logic_program.*;
 public class App {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int num = scanner.nextInt();
 
-		Util.sqrt();
+		String bits = Util.toBinary(num);
+		System.out.println("After Padding:" + bits);
 
 	}
 }
